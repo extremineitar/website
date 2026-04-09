800b39a2ed68dfa1bc285348c31da63bc4f1236a
extremineitar <pedropereira1escola@gmail.com>
2026-04-09 01:05:15 +0100

    v3 ex2

--- script.js
+++ script.js
@@ -67,13 +67,91 @@ document.querySelectorAll(".modal-scroller-background-go-back").forEach(function
     });
 });
 
+function ShowLoader(){
+    document.querySelector("#loader").style.display = "flex";
+}
+
+function HideLoader() {
+    document.querySelector("#loader").style.display = "none";
+}
+
 function AbrirProjeto(projeto) {
     document.querySelector("body").classList.add("modal-open");
     document.querySelector(".modal").classList.add("show");
 
     switch (projeto) {
-        case "scp": document.getElementById("projeto_scp").classList.add("show");break;
-        case "metro": document.getElementById("projeto_metro").classList.add("show");break;
-        case "arab": document.getElementById("projeto_arab").classList.add("show");break;
+        case "scp": document.getElementById("projeto_scp").classList.add("show"); break;
+        case "metro": document.getElementById("projeto_metro").classList.add("show"); break;
+        case "arab": document.getElementById("projeto_arab").classList.add("show"); break;
     }
+}
+
+function AbrirPaginaInicial() {
+    const lst = document.querySelectorAll("#nav-items a");
+    lst.forEach(function (a, index) {
+        if (index == lst.length - 1)
+            a.onclick = AbrirPaginaJogo;
+        else
+            a.onclick = "";
+    });
+
+    TogglePaginas();
+}
+
+function AbrirPaginaJogo() {
+
+    ShowLoader();
+    document.querySelectorAll("#nav-items a").forEach(function (a) {
+        a.onclick = AbrirPaginaInicial;
+    });
+
+    TogglePaginas();
+
+    const appid = 223710;
+
+    fetch("https://cors.io/?url=https://store.steampowered.com/api/appdetails?appids=" + appid)
+    .then(res => res.text())
+    .then(text => {
+        const data = JSON.parse(text);
+        document.querySelector("#descricao-jogo").innerHTML = JSON.parse(data.body)[appid].data.short_description;
+    })
+    .finally(() => {
+        fetch("https://cors.io/?url=" + encodeURIComponent("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=AE7FABCA866FD1F1BB03FCA75925D765&steamid=76561198799239986&format=json&include_played_free_games=true"))
+            .then(res => res.text())
+            .then(text => {
+                const data = JSON.parse(text);
+                document.querySelector("#horas-jogadas").innerHTML = JSON.parse(data.body).response.games[15].playtime_forever / 60;
+
+            })
+            .finally(HideLoader);
+    })
+    .catch((error) => {
+        document.querySelector("#pagina-jogo").innerHTML = `<div style='display: flex; width: 100%; justify-content: center;'><p>ocorreu um erro: ${error.message}</p></div>`;
+    });
+
+}
+
+function TogglePaginas() {
+
+    document.querySelector('.container-paginas').classList.toggle('switch');
+
+    const pagInicial = document.querySelector("#pagina-inicial");
+    const pagJogo = document.querySelector("#pagina-jogo");
+
+    pagInicial.classList.toggle("show-pagina");
+    pagJogo.classList.toggle("show-pagina");
+
+    if (pagInicial.classList.contains("show-pagina"))
+        pagInicial.style.visibility = "visible";
+    else
+        pagJogo.style.visibility = "visible";
+
+
+    setTimeout(() => {
+        if (pagInicial.classList.contains("show-pagina"))
+            pagJogo.style.visibility = "hidden";
+        else
+            pagInicial.style.visibility = "hidden";
+
+    }, 400);
 }
